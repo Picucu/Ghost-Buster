5b8abd15ec42295a4be6cc8a142f121e48593fee
Picucu <33139604+Picucu@users.noreply.github.com>
2023-04-07 02:33:49 -0400

    update 1

--- app.mjs
+++ app.mjs
@@ -1,9 +1,78 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url';
-
+import express from 'express';
+import './db.mjs'
+import mongoose from 'mongoose';
+const  Kitchen = mongoose.model('Kitchen')
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+import url from 'url';
+import path from 'path';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'hbs');
+
+const Brands = mongoose.model('Brand');
+const User = mongoose.model('user');
+const Kitchens = mongoose.model('Kitchen')
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/',async (req, res) =>{
+    let brands = '';
+    try{
+        brands = await Brands.find({}).sort('-createdAt');
+    }catch{
+        brands={};
+    }
+    res.render("table.hbs",{brand:brands});
+})
+app.get('/addnew', (req,res)=>{
+    res.render('newBrand.hbs');
+});
+
+app.post('/addnew', async (req,res)=>{
+    let poster = await User.find({name:"admin"}).catch(err => res.status(500).send('no admin error'));
+    let brand = new Brands({
+        user:[poster[0]['_id']],
+        Locations:[req.body.BrandLocation],
+        name:req.body.BrandName,
+        description:req.body.descp,
+        tscore:50
+    })
+    brand.save().then(saved => res.redirect('/')).catch(err =>res.status(500).send('server error'))
+})
 
 app.listen(process.env.PORT || 3000);
+
+/* 
+code snippet for addin basic markers, I have the api key still figuring out how to do this
+
+just found out that google maps has a webgl version of the map now! I want to integrate this into the site, it looks very cool.
+
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+*/
--- db.mjs
+++ db.mjs
@@ -1,32 +1,40 @@
 import mongoose from "mongoose";
+import slug from 'mongoose-slug-updater';
 
 const mongooseOpts = {
     useNewUrlParser: true,  
     useUnifiedTopology: true
   };
   
-  mongoose.connect('mongodb://localhost/proj_ghost', mongooseOpts).then(console.log('Connected')).catch(err => console.log(err));
+  mongoose.connect('mongodb://127.0.0.1:27017/finalproj', mongooseOpts).then(console.log('Connected')).catch(err => console.log(err));
+
+
+mongoose.plugin(slug);
+
 
 const User = new mongoose.Schema({
-    username:String,
-    password:String,
-    posts:[KitchenSchema,BrandSchema],
+    username:{type: String, required: true, minLength: 3, maxLength: 20},
+    password:{type: String, required: true, minLength: 8},
+    kitchenposts:[{type: mongoose.Schema.Types.ObjectId, ref: 'Kitchen'}],
+    brandposts:[{type: mongoose.Schema.Types.ObjectId, ref: 'Brand'}],
     cscore:Number
 })
 const KitchenSchema = new mongoose.Schema({
-    user:[User],
+    user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     name:String,
-    Brands:[BrandSchema],
-    timestamps: true,
+    Location:String,
+    Brands:[{type: mongoose.Schema.Types.ObjectId, ref: 'Brand'}],
+    slug: {type: String, slug: 'name', unique: true},
     tScore:Number
-})
+},{timestamps: true})
 const BrandSchema = new mongoose.Schema({
-    user:[User],
+    user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
     name:String,
-    Locations:String,
-    timestamps: true,
+    Locations:[{type:String}],
+    description: {type: String, required: false},
+    slug: {type: String, slug: 'name', unique: true},
     tscore:Number
-})
+},{timestamps: true})
 mongoose.model('user', User);
 mongoose.model('Kitchen', KitchenSchema);
 mongoose.model('Brand', BrandSchema);
